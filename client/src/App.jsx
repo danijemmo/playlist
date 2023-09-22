54021d4d5f3d943ab7279a50ccaef5099ea223a1
danijemmo <danieljemmo1@gmail.com>
2023-09-22 12:02:47 -0700

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="">
+      <p className="text-3xl font-bold text-center">Playlist</p>
+    </div>
+  );
+}
